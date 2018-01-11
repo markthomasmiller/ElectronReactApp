1cf426a4e968249677ac8d725270d667242e9a11
Mark Miller <markfromzeph+github@gmail.com>
2018-01-11 11:36:21 -0500

    Added icons and additional branding

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 // Basic init
 const electron = require('electron')
+var path = require('path')
 const { app, BrowserWindow } = electron
 
 // Let electron reload by itself when webpack watches changes in ./app/
-require('electron-reload')(__dirname)
+if (process.env.NODE_ENV === 'development') {
+	require('electron-reload')(__dirname)
+}
 
 // To avoid being garbage collected
 let mainWindow
@@ -12,8 +15,18 @@ app.on('ready', () => {
 	let mainWindow = new BrowserWindow({
 		width: 800,
 		height: 600,
-		titleBarStyle: 'hiddenInset'
-	})
+		title: "ElectronReactApp",
+		titleBarStyle: 'hiddenInset',
+		backgroundColor: '#000000',
+		icon: path.join(__dirname, 'app/src/assets/icons/png/64x64.png'),
+		show: false
+	});
+	
+	mainWindow.once('ready-to-show', () => {
+     mainWindow.show()
+ })
+	
+	mainWindow.setTitle(require('./package.json').name);
 
 	mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 })
