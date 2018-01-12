37629fa2b5810776ae464bb81a3501315f971602
Mark Miller <markfromzeph+github@playonjump.com>
2018-01-12 11:18:54 -0500

    Add scaffolding for menus and window activation

--- main.js
+++ main.js
@@ -1,32 +1,51 @@
-// Basic init
 const electron = require('electron')
-var path = require('path')
+const path = require('path')
 const { app, BrowserWindow } = electron
 
-// Let electron reload by itself when webpack watches changes in ./app/
+// Let Electron reload itself when Webpack watches changes
 if (process.env.NODE_ENV === 'development') {
 	require('electron-reload')(__dirname)
 }
 
-// To avoid being garbage collected
+// Allows app to avoid garbage collection
 let mainWindow
 
-app.on('ready', () => {
-	let mainWindow = new BrowserWindow({
+function createAppInstance () {
+	// Define window properties
+	mainWindow = new BrowserWindow({
 		width: 800,
 		height: 600,
 		title: "ElectronReactApp",
 		titleBarStyle: 'hiddenInset',
 		backgroundColor: '#000000',
 		icon: path.join(__dirname, 'app/src/assets/icons/png/64x64.png'),
-		show: false
-	});
+		show: false,
+		minHeight: 300,
+  	minWidth: 300
+	})
 	
+	// Load window contents
+	mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+	
+	// Create menu
+	require('./app/menu/menu.js')
+	
+	// Show window when ready
 	mainWindow.once('ready-to-show', () => {
      mainWindow.show()
- })
+ 	})
 	
-	mainWindow.setTitle(require('./package.json').name);
+	// Dereference the window object on close
+	mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
 
-	mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+// Creates a new window when activated with no other windows open (Mac)
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createAppInstance()
+  }
 })
+
+app.on('ready', createAppInstance)
